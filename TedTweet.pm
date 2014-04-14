21b8808c5d8c2c28d1b7556e24b836e7d52907c7
Michelle <mguy@usgs.gov>
2014-04-14 11:29:23 -0600

    Update TedTweet.pm
    added disclaimer

--- TedTweet.pm
+++ TedTweet.pm
@@ -2,6 +2,14 @@ package TedTweet;
 use Net::Twitter::Lite;
 
 ###############################################################################
+#  This software is in the public domain because it contains materials
+#  that originally came from the United States Geological Survey, 
+#  an agency of the United States Department of Interior. For more 
+#  information, see the official USGS copyright policy at 
+#  http://www.usgs.gov/visual-id/credit_usgs.html#copyright
+###############################################################################
+
+###############################################################################
 # tweetout:
 #  notes: 
 ###############################################################################
