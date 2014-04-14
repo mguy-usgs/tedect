865a501fc02d52f13c72b1f9ed2c47b2154afc9a
Michelle <mguy@usgs.gov>
2014-04-14 11:28:34 -0600

    Update TedStatus.pm
    added disclaimer

--- TedStatus.pm
+++ TedStatus.pm
@@ -5,6 +5,14 @@ use Date::Parse;
 use Date::Format;
 
 ###############################################################################
+#  This software is in the public domain because it contains materials
+#  that originally came from the United States Geological Survey, 
+#  an agency of the United States Department of Interior. For more 
+#  information, see the official USGS copyright policy at 
+#  http://www.usgs.gov/visual-id/credit_usgs.html#copyright
+###############################################################################
+
+###############################################################################
 # status_generate:
 #  parameters: todays date (maybe previous day?)
 #  return: void
